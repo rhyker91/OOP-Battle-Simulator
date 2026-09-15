d5a3ee2d9177633b9886b27cbf4e6028ccd57978
rhyker91 <rhyker.colontrelle@ascte.org>
2026-09-14 20:43:09 -0500

    Added part of Hero class and second goblin entrance

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from goblin import Goblin
+from hero import Hero
 
 
 ARENA_NAME = "The Iron Lung"
@@ -11,10 +12,14 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Gribble")
+    gobline = Goblin("Kibble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    print(f"{gobline.name} enters the arena with {gobline.health} health.")
     print("But no hero has answered the call... yet.")
 
+    hero = Hero("Bart")
+    print("Hero Name: ",hero.name,"Health: ", hero.health)
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,22 @@
+import random
+
 class Hero:
     """The hero blueprint will be implemented later in the project."""
+    def __init__(self, name):
+        self.name=name
+        self.health=123
+        self.attack_power=21
 
-    pass
+    def attack(self):
+        return random.ranint(1,self.attack_power)
+
+    def take_damage(self, damage):
+        health=self.health-damage
+        if health < 0:
+            health-=health
+        return health
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
